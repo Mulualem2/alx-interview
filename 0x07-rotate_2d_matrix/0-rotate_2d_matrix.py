e12acd33be29b878f5c0e599e3369a0d06ece2e9
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-02-16 21:03:04 +0000

    task added

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Given an n x n 2D matrix, rotate it 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotates the n x n 2d matrix """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
